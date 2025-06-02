81f32a5e397f9c355c95fe71c64041d6293c4432
AslDeveloper07 <aslwexking777@gmail.com>
2025-06-02 13:57:58 +0500

    add

---
+++ javaScript/profile.js
@@ -0,0 +1,50 @@
+"use strict";
+
+const kirdi = document.getElementById("kirdi");
+const chiqdi = document.getElementById("chiqdi");
+const search = document.getElementById("searchInput");
+const balance = document.getElementById("balance");
+const avatar = document.getElementById("avatarka");
+const date = document.getElementById("date");
+const transformInp1 = document.getElementById("input1");
+const transformInp2 = document.getElementById("input2");
+const transformBtn = document.getElementById("transformBtn");
+const setIcon = document.getElementById("set");
+const buyIcon = document.getElementById("buy");
+const requistInp = document.getElementById("requistInp");
+const closeBtn = document.getElementById("closeBtn");
+const time = document.getElementById("time");
+// console.log(
+//   kirdi,
+//   chiqdi,
+//   search,
+//   balance,
+//   avatar,
+//   date,
+//   transformBtn,
+//   transformInp1,
+//   transformInp2,
+//   setIcon,
+//   buyIcon,
+//   requistInp,
+//   closeBtn,
+//   time
+// );
+
+
+const accaunts=[
+    {
+        owner:"Asilbek",
+        ownerImage:'./../resource/img/avatar.jpg',
+        userName: "Asilbekjon",
+        password: 'root2008',
+        balance: 2345236475
+    },
+    {
+        owner:"Azizbek",
+        ownerImage:'./../resource/img/avatar.jpg',
+        userName: "Azizbekjon",
+        password: 'root2010',
+        balance: 3543646546
+    },
+]
