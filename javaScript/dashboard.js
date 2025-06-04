0c224ae379fd7806756992ef7b4535e93a222fd4
AslDeveloper07 <aslwexking777@gmail.com>
2025-06-05 02:10:12 +0500

    dadsa

--- javaScript/dashboard.js
+++ javaScript/dashboard.js
@@ -2,13 +2,40 @@ window.addEventListener("DOMContentLoaded", () => {
   const profileName = document.getElementById("ownerName");
   const logOut = document.getElementById("logOut");
   const balance = document.getElementById("balance");
-  const avatar=document.getElementById("avatarka")
+  const avatar = document.getElementById("avatarka");
+  const ownerName = document.getElementById("ownerName");
+  const modalProfile = document.getElementById("modal");
+  console.log(logOut);
 
-  const dashboard =JSON.parse( localStorage.getItem('accaunts'))
-//   console.log(dashboard);
+  let isModalOpen = false;
 
-if(dashboard){
-    avatar.src=accaunts.ownerImage??'./../resource/img/defaultavatar.jpg'
-}
+  const dashboard = JSON.parse(localStorage.getItem("accaunts"));
+  //   console.log(dashboard);
 
+  if (dashboard) {
+    avatar.src = dashboard?.ownerImage ?? "./../resource/img/defaultavatar.jpg";
+
+    ownerName.textContent = dashboard.owner ?? "Username ";
+    balance.textContent = dashboard.balance ?? "0.00";
+
+    avatar.addEventListener("click", () => {
+      isModalOpen = !isModalOpen;
+
+      if (isModalOpen) {
+        modalProfile.classList.remove("hidden");
+        modalProfile.classList.add("flex");
+      } else {
+        modalProfile.classList.add("hidden");
+        modalProfile.classList.remove("flex");
+      }
+    });
+
+    logOut.addEventListener("click", () => {
+      checkLogOut = confirm("Profiledan chiqishni hohlaysizmi?");
+      if (checkLogOut) {
+        localStorage.removeItem("accaunts");
+        window.location.replace("./../index.html");
+      }
+    });
+  }
 });
